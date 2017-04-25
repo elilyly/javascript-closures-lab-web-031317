4f8dbca52af79d96783c6a4ebeaf04c2211939f3
Elizabeth <elizabethnich19@gmail.com>
2017-04-25 19:25:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    counter += 1
+  }
+  function getBumps() {
+    return counter
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function deadlyAnimal(deadlyDevice) {
+    return {"animalType": animalType,
+    "deadlyDevice": deadlyDevice
+    }
+  }
+  return deadlyAnimal;
+}
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
